7a497e739706a741b694c38685e2beb6b6154db2
ryo <hideyoshi.adm@gmail.com>
2024-09-10 22:40:23 +0900

    【概要】Dockerメンテ 【関連処理】

---
+++ run_docker_build_and_aws_push.bat
@@ -0,0 +1,34 @@
+@echo off
+REM バッチファイルが置かれているディレクトリを取得
+set WSL_DIR=%~dp0
+
+REM ログファイルのパスを指定
+set LOG_FILE=%WSL_DIR%docker_build_and_aws_push.txt
+
+REM ログファイルが存在する場合削除
+if exist "%LOG_FILE%" del "%LOG_FILE%"
+
+REM 実行開始時刻をログに記録
+echo Execution started at %date% %time% >> "%LOG_FILE%"
+echo -------------------------------------------------- >> "%LOG_FILE%"
+
+REM 現在のディレクトリパスをログに出力
+echo Batch file directory: %WSL_DIR% >> "%LOG_FILE%"
+
+echo "■AWSログイン" >> "%LOG_FILE%"
+aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin 990606419933.dkr.ecr.ap-northeast-1.amazonaws.com >> "%LOG_FILE%"
+echo "■ビルド" >> "%LOG_FILE%"
+@REM docker build -t react-frontend -f Dockerfile.prod . >> "%LOG_FILE%"
+docker compose --env-file .env.prod build --no-cache >> "%LOG_FILE%"
+echo "■タグつけ" >> "%LOG_FILE%"
+docker tag react-prod-image:latest 990606419933.dkr.ecr.ap-northeast-1.amazonaws.com/react-frontend:latest >> "%LOG_FILE%"
+echo "■プッシュ" >> "%LOG_FILE%"
+docker push 990606419933.dkr.ecr.ap-northeast-1.amazonaws.com/react-frontend:latest >> "%LOG_FILE%"
+
+REM 実行完了時刻をログに記録
+echo Execution finished at %date% %time% >> "%LOG_FILE%"
+echo -------------------------------------------------- >> "%LOG_FILE%"
+
+REM 実行結果を表示
+echo Docker control script executed in WSL
+
